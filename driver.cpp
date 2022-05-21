5e28b21dff6330ee0ecc91f4fb9c5551ecf8e4d3
Nathan Allers <nateallers@gmail.com>
2022-05-21 12:14:27 -0500

    Delete driver.cpp

--- driver.cpp
+++
@@ -1,9 +0,0 @@
-#include "JobSuggest.h"
-int main(){
-	std::array<jobResult,11> jobs = jobSuggest();
-	for(jobResult job : jobs){
-		double perc = job.percentile*100;
-		cout << job.jobName << ": " << perc << "% Match\n";
-	}
-	return 0;
-}
